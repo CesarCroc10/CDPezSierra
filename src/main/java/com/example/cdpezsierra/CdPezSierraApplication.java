405a0cc0faefd07e198cc285ecf3fbbe6f7cfe6e
unknown <UFG@CC5-32.ufg.local>
2024-11-13 07:41:58 -0600

    Commit inicial con archivos de proyecto

---
+++ src/main/java/com/example/cdpezsierra/CdPezSierraApplication.java
@@ -0,0 +1,13 @@
+package com.example.cdpezsierra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdPezSierraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdPezSierraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cdpezsierra/CdPezSierraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdpezsierra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdPezSierraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
